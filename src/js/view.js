f638ca2c1601e333bfdf6d9905af2d3f6bef41ae
Thomas Muguet <t.muguet@thomasmuguet.info>
2017-10-19 02:36:54 +0200

    Fixed infinite loading if saved zoom level was greater than 18

--- src/js/view.js
+++ src/js/view.js
@@ -23,6 +23,9 @@ L.Map.include({
             let view = $.localStorage.getAsJSON('view') ||
                 [44.96777356135154, 6.06822967529297, 13];   // Center in les Ecrins because I love this place
 
+            if (view[2] > 17)
+                view[2] = 17;
+
             if ('lat' in $.QueryString && 'lng' in $.QueryString) {
                 view = [$.QueryString.lat, $.QueryString.lng, 15];
             }
